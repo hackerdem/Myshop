670e09896c0d5d9127c868d7ed3a8a8e82b4fa89
erdem YILMAZ <werdem@gmail.com>
2018-04-22 20:19:45 +1000

    register

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.django_app.default',
+    'social_django',
     'account',
     'shop',
     'cart',
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Category,Product
 from cart.forms import CartAddProductForm
+from django.http import HttpResponse
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 
 def product_list(request,category_slug=None):
     category=None
     categories=Category.objects.all()
     products=Product.objects.filter(available=True)
+    paginator=Paginator(products,8)
+    page=request.GET.get('page')
+    try:
+        products=paginator.page(page)
+    except PageNotAnInteger:
+        products=paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        products=paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,'shop/product/list_ajax.html',{'products':products})
     if category_slug:
         category=get_object_or_404(Category,slug=category_slug)
         products=products.filter(category=category)
