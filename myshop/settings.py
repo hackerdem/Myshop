955f25a6a20612c13dea63213f90cbd4d0d1450b
erdem YILMAZ <werdem@gmail.com>
2018-05-28 16:21:06 +1000

    updates

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 from django.conf import settings
 from .settings_data import SECRET,FACEBOOK_KEY,FACEBOOK_SECRET, \
             GOOGLE_KEY,GOOGLE_SECRET,TWITTER_KEY,TWITTER_SECRET, \
@@ -20,7 +20,7 @@ from .settings_data import SECRET,FACEBOOK_KEY,FACEBOOK_SECRET, \
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_require=True)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
