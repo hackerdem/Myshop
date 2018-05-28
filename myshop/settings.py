6a92886a0fb1992345da14d7a4297d3a355eb3da
erdem YILMAZ <werdem@gmail.com>
2018-05-28 15:29:11 +1000

    updates

--- myshop/settings.py
+++ myshop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =SECRET
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['artsshop.herokuapp.com','127.0.0.1']
 SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 
 # Application definition
